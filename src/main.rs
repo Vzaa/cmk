78ddea9b9a136b3cdfc616c9b6eeaac3a832f4f9
Vzaa <Vzaa@users.noreply.github.com>
2017-12-08 17:11:15 +0300

    Add --format option

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ use std::process::exit;
 
 use cmk::{Entry, Values};
 
+const DEFFORMAT: &str = "%n(%a): $%i -> $%s (%d)\n=> %1 %2 %7";
+
 fn main() {
     let matches = clap_app!(cmk =>
         (version: "0.1")
         (@arg PROXY: -p --proxy +takes_value "Proxy URL")
         (@arg LIMIT: -l --limit +takes_value "Queried currency limit (Default: 150)")
+        (@arg FORMAT: -f --format +takes_value "Custom format")
         (@arg SUMMARY: -s --summary "Summary only")
         (@arg FILE: +required "Portfolio JSON File")
     ).get_matches();
@@ -22,6 +25,7 @@ fn main() {
     let summary = matches.is_present("SUMMARY");
     let json_path = matches.value_of("FILE").unwrap();
     let proxy: Option<&str> = matches.value_of("PROXY");
+    let format_str: &str = matches.value_of("FORMAT").unwrap_or(DEFFORMAT);
 
     let limit: u32 = matches
         .value_of("JSON")
@@ -45,41 +49,35 @@ fn main() {
         })
         .sum();
 
-    if summary {
-        println!(
-            "${:.2} -> ${:.2} (T {:.2}, 1H {:.2})",
-            sum_init,
-            sum_usd,
-            sum_usd - sum_init,
-            m1);
-    } else {
+    if !summary {
         for e in p {
             let c = coins.get(&e.id).unwrap();
             let Values(val, init, c1, c24, c7) = e.values(&c);
-            println!(
-                "{}({:.2}): ${:.2} -> ${:.2} ({:.2}, {:.2}%)\n=> {:.2} {:.2} {:.2}\n",
-                c.name,
-                e.amount,
-                init,
-                val,
-                val - init,
-                ((val - init) / init) * 100.0,
-                c1,
-                c24,
-                c7
-                );
-        }
+            let out = format_str
+                .to_owned()
+                .replace("%n", &format!("{}", c.name))
+                .replace("%a", &format!("{:.2}", e.amount))
+                .replace("%i", &format!("{:.2}", init))
+                .replace("%s", &format!("{:.2}", val))
+                .replace("%d", &format!("{:.2}", val - init))
+                .replace("%1", &format!("{:.2}", c1))
+                .replace("%2", &format!("{:.2}", c24))
+                .replace("%7", &format!("{:.2}", c7));
 
-        println!(
-            "${:.2} -> ${:.2} ({:.2}, {:.2}%) => {:.2} {:.2} {:.2}",
-            sum_init,
-            sum_usd,
-            sum_usd - sum_init,
-            ((sum_usd - sum_init) / sum_init) * 100.0,
-            m1,
-            m24,
-            m7
-            );
+            println!("{}\n", out);
+        }
     }
 
+    let out = format_str
+        .to_owned()
+        .replace("%n", &format!("{}", "Total"))
+        .replace("%i", &format!("{:.2}", sum_init))
+        .replace("%a", "")
+        .replace("%s", &format!("{:.2}", sum_usd))
+        .replace("%d", &format!("{:.2}", sum_usd - sum_init))
+        .replace("%1", &format!("{:.2}", m1))
+        .replace("%2", &format!("{:.2}", m24))
+        .replace("%7", &format!("{:.2}", m7));
+
+    println!("{}", out);
 }
